76ee1e3cc4c77f545e2520fbd113d75b56e7d6ed
tanpeterson <tanpeterson@gmail.com>
2021-11-21 00:26:44 -0800

    Refactor
    -Removed file creation from compiler and moved it into the build command.
    -Added parameters to build command to allow for testing and benchmarking.
    
    Co-Authored-By: Sylvia Liu <49706079+liusylvia@users.noreply.github.com>

--- cli/commands/build.ts
+++ cli/commands/build.ts
@@ -1,56 +1,52 @@
 import flags from "./flags.ts";
 import { ensureFile } from "https://deno.land/std@0.113.0/fs/mod.ts";
+import { join } from "https://deno.land/std@0.113.0/path/mod.ts";
 import { compiler } from "../../compiler/compiler.ts";
 import boilerplate from "../../templates/build.ts";
 
-const getProjectDir = async (currentPath: string): Promise<string[]> => {
-  const projectNestedDir: string[] = [];
-
-  for await (const dirEntry of Deno.readDir(currentPath)) {
-    // Ignores build/dist folders. Change later to ignore build folder input by user in config.json
-    if (
-      dirEntry.name === "build" ||
-      dirEntry.name === "server"
-    ) {
-      continue;
-    }
-    const entryPath = `${currentPath}/${dirEntry.name}`;
-    projectNestedDir.push(entryPath);
-
-    if (dirEntry.isDirectory) {
-      projectNestedDir.push(...await getProjectDir(entryPath));
-    }
-  }
-  return projectNestedDir;
-};
-
-// Function to copy over user files when given build command
-const buildUserFiles = async (path:string) => { 
-  const projectDir = await getProjectDir(`${path}/src`);
-  const userFiles = projectDir?.filter((file: string) => (!file.includes("/.") || !file.includes('.svelte')) && ((file.endsWith(".ts") || file.endsWith('.js'))) );
-  for (const filePath of userFiles) { 
-    const currentBuildPath = './build' + filePath.replace(`${path}`, '');
-    if (currentBuildPath === './build/src/index.js') continue;
-    
-    const fileContent = await Deno.readFile(filePath);
-    await ensureFile(currentBuildPath);
-    await Deno.writeFile(currentBuildPath, fileContent);
-  }
-}
-
 // Function to run when given build command
-export const BuildProject = async (flag: string, path:string = Deno.cwd()) => {
+export const BuildProject = async (flag: string, cwd = Deno.cwd(), path = '/src/App.svelte') => { // C:\\Users\\Tanner\\Documents\\GitHub\\NOVAS2\\tests\\src\\App.svelte
+  const encoder = new TextEncoder();
+  const fullPath = join(cwd, path);
+  const memoized: {[key: string]: boolean} = {};
+
   if (flags['help'][flag]) {
     console.log(`To run build, type:` + ` %csvno build`, "color:#55dac8");
     return false;
   }
-  const encoder = new TextEncoder();
-  await ensureFile('./build/index.js');
-  const indexjs = encoder.encode(boilerplate.indexJs);
-  Deno.writeFile("./build/index.js", indexjs);
 
-  await buildUserFiles(path);
-  compiler(`${path}/src/App.svelte`);
+  await ensureFile('./build/index.js');
+  Deno.writeFile("./build/index.js", encoder.encode(boilerplate.indexJs));
+  
+  const buildImports = async (filePath: string) => {
+    filePath.endsWith('.svelte') ? await handleSvelte() : handleOther(); 
+
+    async function handleSvelte() {
+      const { js, ast } = await compiler(filePath); 
+      const data = encoder.encode(js);
+      
+      await ensureFile(join("./build", filePath.replace(cwd, '')) + ".js");
+      await Deno.writeFile(join("./build", filePath.replace(cwd, '')) + ".js", data);
+  
+      const nestedImports = ast.instance?.content?.body?.filter((script: { type: string; source: { value: string; }; }) => script.type === "ImportDeclaration")
+      if(!nestedImports) return;
+      for(const nested of nestedImports){
+        if (memoized[nested.source.value] === true) continue;
+        memoized[nested.source.value] = true;
+        buildImports(join(cwd, nested.source.value.replace('.', 'src/'))); 
+      }
+    }
+  
+    async function handleOther(){
+      const data = encoder.encode(filePath);
+      await ensureFile("./build" + filePath.replace(cwd, ''));
+      await Deno.writeFile("./build" + filePath.replace(cwd, ''), data);
+    }
+  }
+  
+  await buildImports(fullPath); 
+
   console.log("Your build was successful!");
   return true;
 }
+
